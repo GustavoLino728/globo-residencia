850061aec90338ed0b19e313ebfaf53a00c2cdd1
Giulliano <expertgiulliano@gmail.com>
2025-10-11 21:15:56 -0300

    config: mudança para utilizar apenas esse repositório

---
+++ frontend/app/(navbar-routes)/relatorios/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import MusicCard from "@/components/blurred_card"
+import SearchIcon from "../../../components/inputSearch"
+
+export default function RelatoriosPage() {
+    return(
+      
+        <div className="px-4">
+            <div className="w-full max-w-[1020px] h-[80vh] sm:h-[815px] mt-[150px] mx-auto sm:mx-0 rounded-tl-[60px] rounded-tr-[60px] bg-white opacity-100" />
+        </div>
+
+    )}
---
+++ frontend/app/(navbar-routes)/upload/page.tsx
@@ -0,0 +1,12 @@
+import AudioUpload from "@/components/inputFile"
+import UploadHistory from '@/components/UploadHistory'
+
+export default function UploadPage() {
+    return(
+        <div>
+            <AudioUpload />
+            <UploadHistory />
+        </div>
+
+    )
+}
