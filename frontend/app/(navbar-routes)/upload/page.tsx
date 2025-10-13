1d39b8d8ad8f4a2f241811c06831614035a0b949
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-10-12 21:20:29 -0300

    refactor: remove UploadHistory component from UploadPage

--- frontend/app/(navbar-routes)/upload/page.tsx
+++ frontend/app/(navbar-routes)/upload/page.tsx
@@ -1,11 +1,9 @@
 import AudioUpload from "@/components/inputFile"
-import UploadHistory from '@/components/uploadHistory'
 
 export default function UploadPage() {
     return(
         <div>
             <AudioUpload />
-            <UploadHistory />
         </div>
 
     )
