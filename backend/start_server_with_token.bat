5a95e5fa78274b5d7ca0b7adc29bb2a158a47a13
Giulliano <expertgiulliano@gmail.com>
2025-10-14 16:34:26 -0300

    delete: remoção dos arquivos que não estão sendo utilizados

--- backend/start_server_with_token.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Start the compiled server with AUDD_TOKEN set for this process
-REM *** IMPORTANTE: Altere o token abaixo para usar um token AudD diferente ***
-REM *** Esta alteração será automaticamente aplicada em todo o sistema ***
-setlocal
-set "AUDD_TOKEN=dedd3859464b7bd712cd83e14be921cd"
-cd /d "%~dp0"
-node dist\server.js
