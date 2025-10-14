7255bfaa98ece3694acad54632097a305e9438d2
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-10-14 12:23:29 -0300

    refactor: simplify ColorBar component by replacing multiple divs with a single gradient background for improved visual appeal

--- frontend/components/coloredFooter.tsx
+++ frontend/components/coloredFooter.tsx
@@ -1,14 +1,6 @@
 export default function ColorBar() {
     return (
-      <div className="fixed bottom-0 left-0 w-full h-2 flex z-50">
-        <div className="flex-1 bg-yellow-400" />
-        <div className="flex-1 bg-orange-500" />
-        <div className="flex-1 bg-red-500" />
-        <div className="flex-1 bg-pink-500" />
-        <div className="flex-1 bg-purple-600" />
-        <div className="flex-1 bg-blue-600" />
-        <div className="flex-1 bg-sky-400" />
-        <div className="flex-1 bg-green-500" />
-      </div>
+      <div className="fixed bottom-0 left-0 right-0 h-1 bg-gradient-to-r from-yellow-400 via-red-500 via-purple-500 via-blue-500 to-green-500 z-50">
+ </div>
     );
   }
