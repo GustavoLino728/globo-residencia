c3dba7ecc569fc8c0b80f2862b5dd7e7e6552b5c
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-10-15 16:55:56 -0300

    refactor: remove colorBar from relatorios

--- frontend/app/(navbar-routes)/relatorios/page.tsx
+++ frontend/app/(navbar-routes)/relatorios/page.tsx
@@ -37,9 +37,6 @@ const Index = () => {
           </div>
         </div>
       </main>
-
-      {/* Bottom gradient accent */}
-      <div className="absolute bottom-0 left-0 right-0 h-1 bg-gradient-to-r from-yellow-400 via-red-500 via-purple-500 via-blue-500 to-green-500"></div>
     </div>
   );
 };
