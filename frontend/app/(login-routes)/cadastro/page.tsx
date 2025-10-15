28728facc9457e6812f9b77af2cb1ef629b54b56
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-10-14 22:15:06 -0300

    feat: add Login component and remove placeholder text from Cadastro page

--- frontend/app/(login-routes)/cadastro/page.tsx
+++ frontend/app/(login-routes)/cadastro/page.tsx
@@ -2,7 +2,6 @@ export default function Cadastro() {
     return(
         <div>           
             <div className="fixed top-0 left-0 h-screen w-1/2 bg-black">
-                teste
             </div>
         </div>
     )
