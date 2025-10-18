af62379b7daea39e759b077563f005e84fd035cb
Giulliano <expertgiulliano@gmail.com>
2025-10-18 15:08:54 -0300

    delete: remoção dos arquivos do front-end na branch backend

--- frontend/app/(login-routes)/cadastro/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Cadastro() {
-    return(
-        <div>           
-            <div className="fixed top-0 left-0 h-screen w-1/2 bg-black">
-                teste
-            </div>
-        </div>
-    )
-}
