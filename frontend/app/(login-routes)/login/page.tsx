88ad54cd9f8c2f97c1338b75fe1a6204d941fb43
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-11-16 18:26:27 -0300

    refactor: Deleta page de login e cadastro

--- frontend/app/(login-routes)/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Login() {
-    return(
-        <div>           
-            <div className="fixed top-0 left-0 h-screen w-1/2 bg-black">
-            </div>
-        </div>
-    )
-}
